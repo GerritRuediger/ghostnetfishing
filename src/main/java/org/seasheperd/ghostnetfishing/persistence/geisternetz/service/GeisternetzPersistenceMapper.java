881c1de2444c7fca7819033440ba25921f11a90e
Gerrit <janickvandersar@MacBook-Air-von-Janick.local>
2025-06-22 09:11:24 +0200

    Application runnable

--- src/main/java/org/seasheperd/ghostnetfishing/persistence/geisternetz/service/GeisternetzPersistenceMapper.java
+++ src/main/java/org/seasheperd/ghostnetfishing/persistence/geisternetz/service/GeisternetzPersistenceMapper.java
@@ -1,11 +1,12 @@
 package org.seasheperd.ghostnetfishing.persistence.geisternetz.service;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.springframework.stereotype.Service;
 import org.seasheperd.ghostnetfishing.domain.geisternetz.model.Geisternetz;
 import org.seasheperd.ghostnetfishing.persistence.geisternetz.model.GeisternetzDataModel;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface GeisternetzPersistenceMapper {
   Geisternetz toGeisternetz(GeisternetzDataModel dataModel);
 
--- src/main/java/org/seasheperd/ghostnetfishing/persistence/user/service/UserPersistenceMapper.java
+++ src/main/java/org/seasheperd/ghostnetfishing/persistence/user/service/UserPersistenceMapper.java
@@ -1,11 +1,12 @@
 package org.seasheperd.ghostnetfishing.persistence.user.service;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.springframework.stereotype.Service;
 import org.seasheperd.ghostnetfishing.domain.user.model.User;
 import org.seasheperd.ghostnetfishing.persistence.user.model.UserDataModel;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserPersistenceMapper {
   User toUser(UserDataModel dataModel);
 
